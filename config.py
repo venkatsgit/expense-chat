af81caf446f16899bff9dedd7af626514f726203
mohan-sellinall <89511847+mohan-sellinall@users.noreply.github.com>
2025-03-03 21:54:06 +0530

    expensive chart api changes

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Database connection details
 db_user = "root"
-db_password = "test@123"
+db_password = "Mohan@2628"
 db_host = "localhost"
-db_name = "expensive"
+db_name = "expense_insights"
 
-select_table = ["transactions"]
+select_table = ["expenses"]
 
 HUGGINGFACEHUB_API_TOKEN = "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"
