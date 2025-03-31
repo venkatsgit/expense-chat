4ac0bc252f53bb0a4189279eb1135d4e7354314e
venkatgit1 <162010453+venkatgit1@users.noreply.github.com>
2025-03-31 16:56:29 +0530

    removed the token

--- expense_category/config.py
+++ expense_category/config.py
@@ -1,3 +1,2 @@
-HUGGINGFACEHUB_API_TOKEN = "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"
 
 FACEBOOK_BART_LARGE_MNLI_CLASSIFIER_URL = "https://router.huggingface.co/hf-inference/models/facebook/bart-large-mnli"
